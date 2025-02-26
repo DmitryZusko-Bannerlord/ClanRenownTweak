8a33d91137d5c318d21a837dd54718fe9e5c8a40
Dmitry Zusko <elxf123@gmail.com>
2025-02-26 22:30:58 +0200

    I've added setting parameter to allow apply clan tier changes for non-player clans

--- ClanRenounTweak/MCM/Settings.cs
+++ ClanRenounTweak/MCM/Settings.cs
@@ -16,33 +16,38 @@ namespace ClanRenounTweak.MCM
         public float PlayerRenounMultiplier { get; set; } = 1;
 
         [SettingPropertyInteger("1 tier renoun", 1, 1000000, "0",
-            HintText = "Amount of renoun to be aquired be player to reach clan tier level 1 (def = 50)", RequireRestart = false, Order = 2)]
+            HintText = "Amount of renoun to be aquired to reach clan tier level 1 (def = 50)", RequireRestart = false, Order = 100)]
         [SettingPropertyGroup("Clan tiers", GroupOrder = 2)]
         public int ClanRenounLevelOne { get; set; } = 50;
 
         [SettingPropertyInteger("2 tier renoun", 1, 1000000, "0",
-            HintText = "Amount of renoun to be aquired be player to reach clan tier level 2 (def = 150)", RequireRestart = false, Order = 3)]
+            HintText = "Amount of renoun to be aquired to reach clan tier level 2 (def = 150)", RequireRestart = false, Order = 110)]
         [SettingPropertyGroup("Clan tiers", GroupOrder = 2)]
         public int ClanRenounLevelTwo { get; set; } = 150;
 
         [SettingPropertyInteger("3 tier renoun", 1, 1000000, "0",
-            HintText = "Amount of renoun to be aquired be player to reach clan tier level 3 (def = 350)", RequireRestart = false, Order = 4)]
+            HintText = "Amount of renoun to be aquired to reach clan tier level 3 (def = 350)", RequireRestart = false, Order = 120)]
         [SettingPropertyGroup("Clan tiers", GroupOrder = 2)]
         public int ClanRenounLevelThree { get; set; } = 350;
 
         [SettingPropertyInteger("4 tier renoun", 1, 1000000, "0",
-            HintText = "Amount of renoun to be aquired be player to reach clan tier level 4 (def = 900)", RequireRestart = false, Order = 5)]
+            HintText = "Amount of renoun to be aquired to reach clan tier level 4 (def = 900)", RequireRestart = false, Order = 130)]
         [SettingPropertyGroup("Clan tiers", GroupOrder = 2)]
         public int ClanRenounLevelFour { get; set; } = 900;
 
         [SettingPropertyInteger("5 tier renoun", 1, 1000000, "0",
-            HintText = "Amount of renoun to be aquired be player to reach clan tier level 5 (def = 2350)", RequireRestart = false, Order = 6)]
+            HintText = "Amount of renoun to be aquired to reach clan tier level 5 (def = 2350)", RequireRestart = false, Order = 140)]
         [SettingPropertyGroup("Clan tiers", GroupOrder = 2)]
         public int ClanRenounLevelFive { get; set; } = 2350;
 
         [SettingPropertyInteger("6 tier renoun", 1, 1000000, "0",
-            HintText = "Amount of renoun to be aquired be player to reach clan tier level 6 (def = 6150)", RequireRestart = false, Order = 7)]
+            HintText = "Amount of renoun to be aquired to reach clan tier level 6 (def = 6150)", RequireRestart = false, Order = 150)]
         [SettingPropertyGroup("Clan tiers", GroupOrder = 2)]
         public int ClanRenounLevelSix { get; set; } = 6150;
+
+        [SettingPropertyBool("Apply only to player",
+            HintText = "If not checked, these requirements also  will be applied for non-player clans", RequireRestart = false, Order = 160)]
+        [SettingPropertyGroup("Clan tiers", GroupOrder = 2)]
+        public bool IsApplyClanTiersOnlyToPlayer { get; set; } = true;
     }
 }
