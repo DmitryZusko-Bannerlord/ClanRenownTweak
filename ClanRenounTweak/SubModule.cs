dbc110840800302947cd09e28ac8515fc1884442
Dmitry Zusko <elxf123@gmail.com>
2025-02-27 21:55:02 +0200

    I've replaced DefaultClanTierModel with custom one, so it uses tweaked clan tier level values depending on settings and active clan.
    GetRequiredRenownForTier doesn't requires clan id check, cuz as far as I know, it is being called only for displaying value on player clan page

--- ClanRenounTweak/SubModule.cs
+++ ClanRenounTweak/SubModule.cs
@@ -1,4 +1,6 @@
-﻿using TaleWorlds.CampaignSystem;
+﻿using ClanRenounTweak.Models;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
@@ -6,22 +8,27 @@ namespace ClanRenounTweak
 {
     class SubModule : MBSubModuleBase
     {
+        private readonly static string _harmonyId = "com.goodhunter.clanrenountweak";
+
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
+
+            var harmony = new Harmony(_harmonyId);
+            harmony.PatchAll();
         }
 
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
         {
             base.OnGameStart(game, gameStarterObject);
 
-            if (!(game.GameType is Campaign)) return;
-            AddModels(gameStarterObject);
+            if (gameStarterObject is CampaignGameStarter)
+                RegisterModels(gameStarterObject);
         }
 
-        protected virtual void AddModels(IGameStarter gameStarterObject)
+        protected virtual void RegisterModels(IGameStarter gameStarterObject)
         {
-
+            gameStarterObject.AddModel(new RenounTweakClanTierModel());
         }
     }
 }
