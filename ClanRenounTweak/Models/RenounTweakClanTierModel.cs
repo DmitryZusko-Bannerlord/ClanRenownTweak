7f6e190d55364af1fc27fae173df9271efc1400c
Dmitry Zusko <elxf123@gmail.com>
2025-02-28 21:23:21 +0200

    Cleaned from garbage

--- ClanRenounTweak/Models/RenounTweakClanTierModel.cs
+++ ClanRenounTweak/Models/RenounTweakClanTierModel.cs
@@ -5,65 +5,6 @@ using TaleWorlds.Core;
 
 namespace ClanRenounTweak.Models
 {
-    /*class RenounTweakClanTierModel : DefaultClanTierModel
-    {
-        private static RenounTweakClanTierModel _instance;
-        public static RenounTweakClanTierModel Instance
-        {
-            get
-            {
-                return _instance ?? (_instance = new RenounTweakClanTierModel());
-            }
-        }
-
-        public RenounTweakClanTierModel()
-        {
-
-        }
-
-        public virtual int GetRequiredRenownForTier(int clanTier, bool isPlayerClan)
-        {
-            var settings = ClanRenounTweakSettings.Instance;
-            if (settings == null) return Campaign.Current.Models.ClanTierModel.GetRequiredRenownForTier(clanTier);
-
-            if (isPlayerClan || !settings.IsApplyClanTiersOnlyToPlayer) return CalculateCustomRenounForTier(settings, clanTier);
-
-            return Campaign.Current.Models.ClanTierModel.GetRequiredRenownForTier(clanTier);
-        }
-
-        protected virtual int CalculateCustomRenounForTier(ClanRenounTweakSettings settings, int clanTier)
-        {
-            switch (clanTier)
-            {
-                case 1:
-                    {
-                        return settings.ClanRenounLevelOne;
-                    }
-                case 2:
-                    {
-                        return settings.ClanRenounLevelTwo;
-                    }
-                case 3:
-                    {
-                        return settings.ClanRenounLevelThree;
-                    }
-                case 4:
-                    {
-                        return settings.ClanRenounLevelFour;
-                    }
-                case 5:
-                    {
-                        return settings.ClanRenounLevelFive;
-                    }
-                case 6:
-                    {
-                        return settings.ClanRenounLevelSix;
-                    }
-            }
-            return 0;
-        }
-    }*/
-
     class RenounTweakClanTierModel : DefaultClanTierModel
     {
         public void RecalculateClanTiers()
